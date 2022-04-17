413743a7bab4ff2c46478c25c5415b4d2c2da6ee
Whit Wu <whit.wu@gmail.com>
2022-04-17 19:10:37 -0400

    Adjust home page

--- GRCCFinal/ClientApp/src/app/app.module.ts
+++ GRCCFinal/ClientApp/src/app/app.module.ts
@@ -25,10 +25,10 @@ import { OrderPizzaComponent } from './order-pizza/order-pizza.component';
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'order-pizza', component: OrderPizzaComponent}
+      { path: '', component: OrderPizzaComponent, pathMatch: 'full' },
+      //{ path: 'counter', component: CounterComponent },
+      //{ path: 'fetch-data', component: FetchDataComponent },
+      //{ path: 'order-pizza', component: OrderPizzaComponent}
     ])
   ],
   providers: [],
